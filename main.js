9c405a333f7d4c3af58742ae0b98000bd0ed564a
WESLEY PLAZA <69492841+wapdrums@users.noreply.github.com>
2023-04-04 09:04:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto bloco=laço de repetição
-//while recebe verdadeiro ou falso
-while(contador < 9) {
-    listaDeTeclas[contador].onclick = tocaSom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    contador = contador +1
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-    console.log(contador)
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
-/*
-
-Esse codigo funciona porem teria que repetir para todos os 'sons', ficaria inviavel e dificil de trabalhar, vamos trabalhar com listas para melhorar a legibilidade.
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-*/
